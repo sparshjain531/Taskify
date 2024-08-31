dc747cb967b97050fdf4117adf0d919d402360de
Sparsh Jain <utkarshj531@gmail.com>
2024-08-31 15:37:57 +0530

    fixed engine

--- data/database.js
+++ data/database.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB=()=>{
     mongoose.connect(process.env.MONGO_URI,
         {dbName:"backendbysparsh"})
-        .then(()=>{console.log("Connected With Database")})
+        .then((c)=>{console.log(`Connected With Database with ${c.connection.host}`)})
         .catch(()=>{console.log("Not connected With Database")})
 }
 
